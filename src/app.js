91cf35189cda89a4ee350b1f78e75618f1a5cd20
Keith Whitson <keith.whitson@franka.de>
2021-12-07 23:23:06 +0100

    intermediate changes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-import {of} from 'rxjs';
+import {of, fromEvent} from 'rxjs';
 import {map} from 'rxjs/operators';
 
 of(1, 3, 4).pipe(
     map( x => x * x)
-).subscribe( data => console.log(data))
+).subscribe( data => console.log(data));
+
