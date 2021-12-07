3e8ead2f65d09578883e996d406b281b7b3a9c37
Keith Whitson <keith.whitson@franka.de>
2021-12-07 23:30:30 +0100

    next steps

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import {of, fromEvent} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {map, scan} from 'rxjs/operators';
 
 of(1, 3, 4).pipe(
     map( x => x * x)
 ).subscribe( data => console.log(data));
 
+fromEvent(document, 'click').pipe(
+    scan(count => count + 1, 0)
+).subscribe(count => {
+    console.log('fromEvent',count);
+    console.log(`Clicked ${count} times`)
+})
+
+let cnt = 0;
+document.addEventListener('click', (event) => {
+    console.log(event)
+    cnt = cnt + 1;
+    console.log('eventListener',cnt);
+})
