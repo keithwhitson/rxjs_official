412deafb5528eaa2a0ce90ca25027d73b5d2f0a1
Keith Whitson <keith.whitson@franka.de>
2021-12-08 00:52:57 +0100

    new changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import {of, fromEvent} from 'rxjs';
+import {of, fromEvent, from, Observable} from 'rxjs';
 import {map, scan} from 'rxjs/operators';
 
 of(1, 3, 4).pipe(
@@ -18,3 +18,48 @@ document.addEventListener('click', (event) => {
     cnt = cnt + 1;
     console.log('eventListener',cnt);
 })
+
+const arraySource = from([1,2,3,4,5]);
+arraySource.subscribe(val => console.log("arraySource",val));
+
+const arraySources = of(1,2,3,4,5);
+arraySources.subscribe(val => console.log("arraySources",val));
+
+const observable = new Observable(subscriber => {
+    subscriber.next(1);
+    subscriber.next(2);
+    throw 'This is an error';
+    subscriber.next(3);
+    setTimeout( () => {
+        subscriber.next(4);
+        subscriber.complete();
+    }, 1000);
+});
+
+observable.subscribe(
+    val => console.log("observable",val),
+    err => console.log(err),
+    () => {console.log('complete')}
+);
+
+document.body.innerHTML = '<p>Loading</p>';
+
+const myPromise = new Promise((resolve, reject ) => {
+    console.log('Creating promise');
+    setTimeout( () => {
+        resolve({
+            title: 'WebDev',
+            message: 'WebDev is the best!',
+            age: 12
+        })
+    }, 3000)
+});
+
+const observableFromPromise = from(myPromise);
+
+observableFromPromise.subscribe( data => {
+    document.body.innerHTML = '';
+    for(let key in data){
+        document.body.insertAdjacentHTML('beforebegin','<p>'+data[key]+'</p>')
+    }
+});
