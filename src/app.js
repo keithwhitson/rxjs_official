272ed0aa9c3605f7663f081a12ec35c8a3e26f00
Keith Whitson <keith.whitson@franka.de>
2021-12-08 01:39:52 +0100

    final changes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import {of, fromEvent, from, Observable, interval, range} from 'rxjs';
-import {map, scan, take, pluck} from 'rxjs/operators';
+import {map, scan, take, pluck, mergeWith, concatWith, delay, mergeMap, switchMap, exhaustMap, concatMap} from 'rxjs/operators';
 
 of(1, 3, 4).pipe(
     map( x => x * x)
@@ -113,3 +113,36 @@ const observableFromPromise3 = from(myPromise3);
 observableFromPromise3.pipe( pluck('age')).subscribe( data => {
     document.body.insertAdjacentHTML('beforebegin','<p> Plucked Age: '+data+'</p>')
 });
+
+
+// let example = concatWith(
+//     of(1).pipe(delay(1000)),
+//     of(2),
+//     of(3)
+// );
+
+// example.subscribe( val => console.log(val))
+
+// example.subscribe(val => console.log(val))
+
+fromEvent(document, 'click').pipe(
+    mergeMap(() => interval(1000))
+).subscribe(val => console.log('mergeMap: ',val));
+
+
+
+fromEvent(document, 'click').pipe(
+    switchMap(() => interval(1000))
+).subscribe(val => console.log('switchMap: ',val));
+
+fromEvent(document, 'click').pipe(
+    exhaustMap(() => interval(1000).pipe(take(5)))
+).subscribe(val => console.log('exhaustMap: ',val));
+
+fromEvent(document, 'click').pipe(
+    concatMap(() => interval(1000).pipe(take(5)))
+).subscribe(val => console.log('concatMap: ',val));
+
+fromEvent(document, 'click').pipe(
+    mergeMap(() => interval(1000))
+).subscribe(val => console.log('mergeMap: ',val));
